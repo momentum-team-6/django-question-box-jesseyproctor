26dee936168e361c34ad8c3fee779ffaf7f1c2fe
jesseyproctor <jss.proctor@gmail.com>
2021-01-10 13:40:15 -0500

    add urls and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Httpresponse
 
 # Create your views here.
+def homepage(request):
+    return Httpresponse("homepage")
+
+def add_user(request):
+    return Httpresponse("hi")
+
+def user_login(request):
+    return Httpresponse("k")
+
+def user_page(request):
+    return Httpresponse("p")
